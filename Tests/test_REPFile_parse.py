05bc7498dae48cd65404b4e04c1d97d36514ffb7
Ian Mayo <ian@planetmayo.com>
2019-11-01 15:22:26 +0000

    introduce first test (#5)
    * introduce first test
    
    * try some diagnostics
    
    * correct Python version
    
    * remove diagnostic lines
    
    * Run all tests in Tests folder
    
    * Verbose test reporting
    
    * -s parameter unnecessary
    
    * Refactor test into two files (to check we discover all tests)

--- Tests/test_REPFile_parse.py
+++ Tests/test_REPFile_parse.py
@@ -5,11 +5,6 @@ import datetime
 from Formats.REPFile import REPFile, REPLine
 from Formats.Location import Location
 
-path = os.path.abspath(__file__)
-dir_path = os.path.dirname(path)
-TEST_FILE = os.path.join(dir_path, "reptest1.rep")
-ERROR_TEST_FILE = os.path.join(dir_path, "reptest2.rep")
-
 class BasicTests(unittest.TestCase):
 
     ############################
@@ -21,40 +16,6 @@ class BasicTests(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-    ####################
-    #### file tests ####
-    ####################
-
-    def test_fileNotFound_excep(self):
-        excep_thrown = False
-        try:
-            REPFile("nonexistant.rep")
-        except IOError:
-            excep_thrown = True
-
-        self.assertTrue(excep_thrown)
-
-    def test_getFileType(self):
-        repfile = REPFile(TEST_FILE)
-        self.assertEqual("REP", repfile.getDatafileType())
-
-    def test_getFileName(self):
-        repfile = REPFile(TEST_FILE)
-        self.assertEqual(TEST_FILE, repfile.getDatafileName())
-
-    def test_getAllLines(self):
-        repfile = REPFile(TEST_FILE)
-        self.assertEqual(8, len(repfile.getLines()))
-
-    def test_fileParseError(self):
-        excep_thrown = False
-        try:
-            REPFile(ERROR_TEST_FILE)
-        except Exception:
-            excep_thrown = True
-
-        self.assertTrue(excep_thrown)
 
     ####################
     #### line tests ####
