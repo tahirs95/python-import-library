880c985dbc2c29326844f985e9b8e2a7107c537c
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-02 13:50:49 +0300

    sqlite database converted to in-memory

--- Experiments/DataStore_initialise_experiment.py
+++ Experiments/DataStore_initialise_experiment.py
@@ -1,7 +1,7 @@
 from Store.DataStoreModule import DataStore
 from Resolvers.CommandLineResolver import CommandLineResolver
 
-data_store = DataStore("", "", "", 0, "sqlite_datastore.db", db_type='sqlite',
+data_store = DataStore("", "", "", 0, ":memory:", db_type='sqlite',
                        missing_data_resolver=CommandLineResolver())
 data_store.initialise()
 
