070d7feeefd6a2e61a421d5147eb6957db6825c8
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-02 23:37:57 +0300

    blacken the code

--- Tests/tests_DataStore_initialise.py
+++ Tests/tests_DataStore_initialise.py
@@ -6,13 +6,7 @@ from sqlalchemy import create_engine
 
 
 class TestDataStoreInitialise(unittest.TestCase):
-
     def setUp(self):
-        self.data_store_sqlite = DataStore("", "", "", 0, ":memory:",
-                                      db_type="sqlite")
-        self.data_store_postgres = DataStore("postgres", "postgres", "localhost",
-                                        "5432", "postgres")
-
         self.postgres = Postgresql(port=5432)
 
     def tearDown(self):
@@ -20,15 +14,15 @@ class TestDataStoreInitialise(unittest.TestCase):
 
     def test_sqlite_initialise(self):
         """Test whether schemas created successfully on SQLite"""
-        pass
+        data_store_sqlite = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
 
     def test_postgres_initialise(self):
-        # TODO: not tested yet because of pip3 library issues
+        # TODO: not working yet
         """Test whether schemas created successfully on PostgresSQL"""
-        query = (
-            "select schema_name",
-            "from information_schema.schemata;"
+        data_store_postgres = DataStore(
+            "postgres", "postgres", "localhost", "5432", "postgres"
         )
+        query = ("select schema_name", "from information_schema.schemata;")
         engine = create_engine(self.postgres.url())
         with engine.raw_connection() as conn:
             cursor = conn.cursor()
@@ -37,12 +31,11 @@ class TestDataStoreInitialise(unittest.TestCase):
 
         self.assertNotIn("datastore_schema", schemas_before)
 
-        self.data_store_postgres.initialise()
+        data_store_postgres.initialise()
         with engine.raw_connection() as conn:
             cursor = conn.cursor
             cursor.execute(query)
             schemas_after = cursor.fetchall()
 
-        self.assertEqual(schemas_before+1, schemas_after)
+        self.assertEqual(schemas_before + 1, schemas_after)
         self.assertIn("datastore_schema", schemas_after)
-
