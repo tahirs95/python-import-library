16123ed46fc8a7727af4e722ebe11713bbfbb360
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-02 14:39:35 +0300

    postgres test developed

--- Tests/tests_DataStore_initialise.py
+++ Tests/tests_DataStore_initialise.py
@@ -13,17 +13,36 @@ class TestDataStoreInitialise(unittest.TestCase):
         self.data_store_postgres = DataStore("postgres", "postgres", "localhost",
                                         "5432", "postgres")
 
-        # self.postgres = Postgresql(port=5432)
+        self.postgres = Postgresql(port=5432)
 
-    # def tearDown(self):
-    #     self.postgres.stop()
+    def tearDown(self):
+        self.postgres.stop()
 
     def test_sqlite_initialise(self):
         """Test whether schemas created successfully on SQLite"""
         pass
 
     def test_postgres_initialise(self):
+        # TODO: not tested yet because of pip3 library issues
         """Test whether schemas created successfully on PostgresSQL"""
+        query = (
+            "select schema_name",
+            "from information_schema.schemata;"
+        )
+        engine = create_engine(self.postgres.url())
+        with engine.raw_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            schemas_before = cursor.fetchall()
 
-        # engine = create_engine(self.postgres.url())
-        pass
+        self.assertNotIn("datastore_schema", schemas_before)
+
+        self.data_store_postgres.initialise()
+        with engine.raw_connection() as conn:
+            cursor = conn.cursor
+            cursor.execute(query)
+            schemas_after = cursor.fetchall()
+
+        self.assertEqual(schemas_before+1, schemas_after)
+        self.assertIn("datastore_schema", schemas_after)
+
