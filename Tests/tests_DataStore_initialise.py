0690e5d7f99b34f63ac720211671870bd6717f97
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-02 23:53:56 +0300

    comments added to tests

--- Tests/tests_DataStore_initialise.py
+++ Tests/tests_DataStore_initialise.py
@@ -9,13 +9,22 @@ class TestDataStoreInitialise(unittest.TestCase):
     def test_sqlite_initialise(self):
         """Test whether schemas created successfully on SQLite"""
         data_store_sqlite = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+
+        # inspector makes it possible to load lists of schema, table, column
+        # information for the sqlalchemy engine
         inspector = inspect(data_store_sqlite.engine)
         table_names = inspector.get_table_names()
+
+        # there must be no table at the beginning
         self.assertEqual(len(table_names), 0)
 
+        # creating database from schema
         data_store_sqlite.initialise()
+
         inspector = inspect(data_store_sqlite.engine)
         table_names = inspector.get_table_names()
+
+        # 11 tables must be created. A few of them tested
         self.assertEqual(len(table_names), 11)
         self.assertIn("Entry", table_names)
         self.assertIn("Platforms", table_names)
@@ -28,18 +37,25 @@ class TestDataStoreInitialise(unittest.TestCase):
         data_store_postgres = DataStore(
             "postgres", "postgres", "localhost", "5432", "postgres"
         )
+
+        # inspector makes it possible to load lists of schema, table, column
+        # information for the sqlalchemy engine
         inspector = inspect(data_store_postgres.engine)
         table_names = inspector.get_table_names()
         schema_names = inspector.get_schema_names()
 
+        # there must be no table, and no schema for datastore at the beginning
         self.assertEqual(len(table_names), 0)
         self.assertNotIn("datastore_schema", schema_names)
 
+        # creating database from schema
         data_store_postgres.initialise()
+
         inspector = inspect(data_store_postgres.engine)
         table_names = inspector.get_table_names()
         schema_names = inspector.get_schema_names()
 
+        # 11 tables must be created. A few of them tested
         self.assertEqual(len(table_names), 11)
         self.assertIn("Entry", table_names)
         self.assertIn("Platforms", table_names)
@@ -47,4 +63,5 @@ class TestDataStoreInitialise(unittest.TestCase):
         self.assertIn("Datafiles", table_names)
         self.assertIn("Nationalities", table_names)
 
+        # datastore_schema must be created
         self.assertIn("datastore_schema", schema_names)
