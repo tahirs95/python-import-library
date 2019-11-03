82ab0c38916beef8b9ba8f52d926ea7a7895e46d
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-03 11:31:24 +0300

    tests divided into two classes

--- Tests/tests_DataStore_initialise.py
+++ Tests/tests_DataStore_initialise.py
@@ -1,11 +1,10 @@
-import unittest
-
+from unittest import TestCase
 from testing.postgresql import Postgresql
 from Store.DataStoreModule import DataStore
 from sqlalchemy import inspect
 
 
-class TestDataStoreInitialise(unittest.TestCase):
+class TestDataStoreInitialisePostgres(TestCase):
     def setUp(self):
         self.postgres = Postgresql(
             database="test", host="localhost", user="postgres", port=55527
@@ -13,32 +12,6 @@ class TestDataStoreInitialise(unittest.TestCase):
 
     def tearDown(self):
         self.postgres.stop()
-
-    def test_sqlite_initialise(self):
-        """Test whether schemas created successfully on SQLite"""
-        data_store_sqlite = DataStore("", "", "", 0, "test.db", db_type="sqlite")
-
-        # inspector makes it possible to load lists of schema, table, column
-        # information for the sqlalchemy engine
-        inspector = inspect(data_store_sqlite.engine)
-        table_names = inspector.get_table_names()
-
-        # there must be no table at the beginning
-        self.assertEqual(len(table_names), 0)
-
-        # creating database from schema
-        data_store_sqlite.initialise()
-
-        inspector = inspect(data_store_sqlite.engine)
-        table_names = inspector.get_table_names()
-
-        # 11 tables must be created. A few of them tested
-        self.assertEqual(len(table_names), 11)
-        self.assertIn("Entry", table_names)
-        self.assertIn("Platforms", table_names)
-        self.assertIn("State", table_names)
-        self.assertIn("Datafiles", table_names)
-        self.assertIn("Nationalities", table_names)
 
     def test_postgres_initialise(self):
         """Test whether schemas created successfully on PostgresSQL"""
@@ -77,3 +50,31 @@ class TestDataStoreInitialise(unittest.TestCase):
 
         # datastore_schema must be created
         self.assertIn("datastore_schema", schema_names)
+
+
+class TestDataStoreInitialiseSQLite(TestCase):
+    def test_sqlite_initialise(self):
+        """Test whether schemas created successfully on SQLite"""
+        data_store_sqlite = DataStore("", "", "", 0, "test.db", db_type="sqlite")
+
+        # inspector makes it possible to load lists of schema, table, column
+        # information for the sqlalchemy engine
+        inspector = inspect(data_store_sqlite.engine)
+        table_names = inspector.get_table_names()
+
+        # there must be no table at the beginning
+        self.assertEqual(len(table_names), 0)
+
+        # creating database from schema
+        data_store_sqlite.initialise()
+
+        inspector = inspect(data_store_sqlite.engine)
+        table_names = inspector.get_table_names()
+
+        # 11 tables must be created. A few of them tested
+        self.assertEqual(len(table_names), 11)
+        self.assertIn("Entry", table_names)
+        self.assertIn("Platforms", table_names)
+        self.assertIn("State", table_names)
+        self.assertIn("Datafiles", table_names)
+        self.assertIn("Nationalities", table_names)
